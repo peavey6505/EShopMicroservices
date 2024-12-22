c3df164f9f536ee77374b5c9cf999e51a97e9f6b
TomaszLenart <t.lenart234@gmail.com>
2024-12-22 10:32:50 +0100

    Get products pagination

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -9,4 +9,5 @@ global using FluentValidation;
 global using BuildingBlocks.Behaviours;
 global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Data;
+global using Marten.Pagination;
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,9 +1,10 @@
 ﻿
+
 namespace Catalog.API.Products.GetProducts
 {
     //best practice - always define query command and result class in a handler class // arguable
 
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
     public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -13,7 +14,8 @@ namespace Catalog.API.Products.GetProducts
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
             return new GetProductsResult(products);
         }
