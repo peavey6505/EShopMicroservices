d8dc003f965a464f47a7489e59640a49c7e2d305
TomaszLenart <t.lenart234@gmail.com>
2024-12-22 20:14:48 +0100

    Delete, Store Basket

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,10 +1,24 @@
-﻿using Basket.API.Models;
+﻿namespace Basket.API.Basket.DeleteBasket;
 
-namespace Basket.API.Basket.DeleteBasket
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
-
-
-    public class DeleteBasketHandler
+    public DeleteBasketCommandValidator()
     {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: delete basket from database and cache       
+        //await repository.DeleteBasket(command.UserName, cancellationToken);
+
+        return new DeleteBasketResult(true);
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,29 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class StoreBasketHandler
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            //TODO: store db. update cache
+
+            return new StoreBasketResult("TestUser");
+
+
+        }
     }
 }
