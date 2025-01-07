b4f538538908b430600a988b839a80d4cfa10dc1
TomaszLenart <t.lenart234@gmail.com>
2025-01-07 14:37:29 +0100

    OrderingAPI

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
-    public record GetOrderByNameQuery(string Name) : IQuery<GetOrdersByNameResult>;
+    public record GetOrdersByNameQuery(string Name) : IQuery<GetOrdersByNameResult>;
 
     public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
 
