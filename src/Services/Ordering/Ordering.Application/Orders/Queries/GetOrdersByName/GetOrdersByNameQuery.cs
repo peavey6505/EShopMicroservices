a524c2ce62e88e62061ec616259921b133bfe607
TomaszLenart <t.lenart234@gmail.com>
2025-01-07 12:47:56 +0100

    Dispatching events, getOrdersByName

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public record GetOrderByNameQuery(string Name) : IQuery<GetOrdersByNameResult>;
+
+    public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
+
+}
